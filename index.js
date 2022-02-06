c9f0d632e53d1e22e09327b6904a61480932a692
christinekarimi <karimikim3@gmail.com>
2022-02-06 14:27:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+let messages = [];
+
+function writeCards(names){
+    for(let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        
+    }
+    return messages;
+}
+function countDown() {
+    let countDown = 10;
+    while ( countDown > -1 ) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
+
+countDown();
